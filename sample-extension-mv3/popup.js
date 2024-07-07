3c4b925b1577d86462dcb81d4bd05abbba97a61b
Balazs Gyurak <ba32107@gmail.com>
2024-07-07 09:10:51 +0100

    Paid & subscription status issue repro

--- sample-extension-mv3/popup.js
+++ sample-extension-mv3/popup.js
@@ -2,17 +2,24 @@
 // registered on ExtensionPay.com to test payments. You may need to
 // uninstall and reinstall the extension to make it work.
 // Don't forget to change the ID in background.js too!
-const extpay = ExtPay('sample-extension') 
+const extpay = ExtPay('<redacted>')
 
-document.querySelector('button').addEventListener('click', extpay.openPaymentPage)
+function populate() {
+    extpay.getUser().then(user => {
+        document.querySelector('textarea').textContent = JSON.stringify(user, null, 2)
+    }).catch(err => {
+        document.querySelector('textarea').textContent = "Error fetching data :( Check that your ExtensionPay id is correct and you're connected to the internet"
+    })
+}
 
-extpay.getUser().then(user => {
-    if (user.paid) {
-        document.querySelector('p').innerHTML = 'User has paid! 🎉'
-        document.querySelector('button').remove()
-    }
-}).catch(err => {
-    document.querySelector('p').innerHTML = "Error fetching data :( Check that your ExtensionPay id is correct and you're connected to the internet"
+document.getElementById('trial').addEventListener('click', extpay.openTrialPage)
+document.getElementById('payments').addEventListener('click', extpay.openPaymentPage)
+document.getElementById('refresh').addEventListener('click', populate)
+document.getElementById('logout').addEventListener('click', async () => {
+    await chrome.storage.sync.remove('extensionpay_installed_at')
+    await chrome.storage.sync.remove('extensionpay_api_key')
+    await chrome.storage.sync.remove('extensionpay_user')
+    populate()
 })
-    
-// extpay.onPaid(function() { console.log('popup paid')});
+
+populate()
